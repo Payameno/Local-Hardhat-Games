390db03ca88b04b4000506a8a76daedb39126a91
Payam Beigi <beigi.payam@gmail.com>
2023-01-03 23:34:57 -0800

    Won forth game

--- scripts/win4.js
+++ scripts/win4.js
@@ -1,13 +1,14 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x0165878A594ca255338adfa4d48449f69242Eb8F";
-const contractName = "Game3";
+const gameAddr = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
+const contractName = "Game4";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-
-const tx = await game.win(45);
+//(uint8 => values between 0 and 255)
+//210 + 56 =< 45 to reach 255. Then begins again from 0 and continues to reach 10 (11)
+const tx = await game.win(56);
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
