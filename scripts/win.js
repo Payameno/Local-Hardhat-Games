4041d29adcde7b6a3c5a9db7c520d15dde9186ee
Payam Beigi <beigi.payam@gmail.com>
2023-01-03 23:02:10 -0800

    Won second game by performing three transactions

--- scripts/win.js
+++ scripts/win.js
@@ -1,12 +1,18 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "";
-const contractName = "Game1";
+const gameAddr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
-    // do whatever you need to do to win the game here:
+
+    const tx1 = await game.setX(25);
+    await tx1.wait();
+
+    const tx2 = await game.setY(25);
+    await tx2.wait();
+
     const tx = await game.win();
 
     // did you win? Check the transaction receipt!
